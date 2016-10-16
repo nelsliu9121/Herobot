e94db536a0846193e253382c8974a5ce06ea8608
Yonsh Lin <yonsh@live.com>
2016-10-16 20:16:36 +0800

    Add Markdown message support

--- scripts/anonymous.js
+++ scripts/anonymous.js
@@ -5,10 +5,16 @@
 //   HUBOT_TELEGRAM_ANONYMOUS_ROOM_ID: required, target room id
 
 var roomId = process.env.HUBOT_TELEGRAM_ANONYMOUS_ROOM_ID;
+var botName = process.env.TELEGRAM_BOT_NAME;
+var botNameLength = botName ? botName.length : 0;
 
 if (roomId != null) {
   module.exports = function(robot) {
-    var flag = [];   // 0: None, 1: Sticker
+    var flag = [];   // 0: None, 1: Sticker, 2: Markdown
+
+    function purifyMessage(msg) {
+      return msg.substring(botNameLength + 1);
+    }
 
     robot.listen(function(msg) {
       return flag[msg.user.id] != null && flag[msg.user.id] != 0;
@@ -22,7 +28,7 @@ if (roomId != null) {
               'telegram:invoke',
               'sendSticker', {
                 chat_id: roomId,
-                sticker: sticker,
+                sticker: sticker
               }, function (error, response) {
                 if (error) {
                   robot.logger.error(error);
@@ -33,11 +39,32 @@ if (roomId != null) {
           } catch (err) {
             console.log(err);
             res.send("Anonymous Sticker: Error, please try again.");
-          } finally {
-            flag[res.message.user.id] = 0;
+          }
+          break;
+        case 2:
+          try {
+            var text = purifyMessage(res.message.text);
+
+            robot.emit(
+              'telegram:invoke',
+              'sendMessage', {
+                chat_id: roomId,
+                text: text,
+                parse_mode: 'Markdown'
+              }, function (error, response) {
+                if (error) {
+                  robot.logger.error(error);
+                }
+                robot.logger.debug(response);
+            });
+
+          } catch (err) {
+            console.log(err);
+            res.send("Anonymous MarkDown Message: Error, please try again.");
           }
           break;
       }
+      flag[res.message.user.id] = 0;
     });
 
     robot.respond(/anon /i, function(res){
@@ -72,6 +99,35 @@ if (roomId != null) {
       flag[res.message.user.id] = 1;
 
       res.send("Anonymous Sticker: OK, please send a sticker to me.")
+
+    });
+
+    robot.respond(/anonmarkdown/i, function(res){
+      if (roomId === res.message.room) {
+        return;
+      }
+
+      flag[res.message.user.id] = 2;
+
+      robot.emit(
+        'telegram:invoke',
+        'sendMessage', {
+          chat_id: res.message.room,
+          text: "Anonymous MarkDown Message: OK, please send a MarkDown message.\n\n" +
+                "*bold text*\n" +
+                "_italic text_\n" +
+                "[text](http://www.example.com/)\n" +
+                "`inline fixed-width code`\n" +
+                "```text\n" +
+                "pre-formatted fixed-width code block\n" +
+                "```",
+          disable_web_page_preview: 'true'
+        }, function (error, response) {
+          if (error) {
+            robot.logger.error(error);
+          }
+          robot.logger.debug(response);
+      });
 
     });
   }
