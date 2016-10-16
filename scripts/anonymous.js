a36c040c072f7f22cf853611275173322cbf0df8
Yonsh Lin <yonsh@live.com>
2016-10-16 15:38:14 +0800

    Initial commit of feature "Anonymous"

---
+++ scripts/anonymous.js
@@ -0,0 +1,30 @@
+// Description:
+//   Anonymously send post to certain room
+//
+// Configuration:
+//   HUBOT_TELEGRAM_ANONYMOUS_ROOM_ID: required, target room id
+
+var roomId = process.env.HUBOT_TELEGRAM_ANONYMOUS_ROOM_ID;
+
+if (roomId != null) {
+  module.exports = function(robot) {
+    robot.respond(/anon (.*)/i, function(res){
+      var msg = res.match[1];
+
+      if (msg !== '') {
+        robot.emit(
+          'telegram:invoke',
+          'sendMessage', {
+            chat_id: roomId,
+            text: msg,
+          }, function (error, response) {
+            if (error) {
+              robot.logger.error(error);
+            }
+            robot.logger.debug(response);
+        });
+      }
+
+    });
+  }
+}
