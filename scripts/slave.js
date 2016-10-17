db6f44d057feefe1025d1ea8b99c996ea615e620
Yonsh Lin <yonsh@live.com>
2016-10-17 14:21:08 +0800

    Add whosyourdaddy

--- scripts/slave.js
+++ scripts/slave.js
@@ -4,12 +4,16 @@
 module.exports = function(robot) {
   robot.respond(/slave/i, function(res){
     var msg;
-    if (res.message.user.name === "YonshLin") {
+    if (res.message.user.name === "YonshLin" || res.message.user.name === "nelsliu") {
       msg = "Your wish is my command, Master " + res.message.user.first_name + ".";
     } else {
       msg = "YOU'RE NO MASTER OF MINE, WORM!";
     }
 
     res.send(msg);
+  });
+
+  robot.respond(/whosyourdaddy/i, function(res){
+    res.send("Nelson is my daddy.");
   });
 }
